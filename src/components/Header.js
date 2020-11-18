d8fd26b9cce8f53e092d758deb7bbccc596a09a7
Carlos Cañellas Tovar <ratiosu.cct@gmail.com>
2020-11-18 11:44:25 +0100

    Fix error on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from "next/link";
 import styled from "styled-components";
-import tw from 'twin.macro';
 
 export default function Header(props) {
   return (
@@ -29,34 +28,4 @@ export default function Header(props) {
         </nav>
   </header>
   )
-  /*
-    <HeaderStyled className="header">
-      <nav
-        className="nav"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <Link href="/">
-          <a><h1>{props.siteTitle || "Carlos Cañellas"}</h1></a>
-        </Link>
-          <Link href={`${typeof window !== "undefined" &&
-          window.location.pathname !== "/" ?
-          "/" : "/about"}`}>
-            <LinkButton>
-            {`${typeof window !== "undefined" &&
-          window.location.pathname !== "/" ?
-          "Inicio" : "Sobre mí"}`}
-            </LinkButton>
-          </Link>
-      </nav>
-    </HeaderStyled>
-  ); */
 }
-
-const HeaderStyled = styled.header`
-  ${tw`bg-gray-800 w-2/3 m-3 mx-auto text-white text-center p-5 rounded-md shadow-md`}
-`
-
-const LinkButton = styled.a`
-  ${tw`bg-blue-700 hover:bg-blue-900 cursor-pointer text-white font-bold py-2 px-4 border-0 rounded`}
-`
